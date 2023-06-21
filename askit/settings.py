074fe78a3e7b0f18c886339bee538352782e6504
JomanDiec <jobiochem@yahoo.com>
2023-06-20 20:13:45 -0700

    heroku deploy-database settings added

--- askit/settings.py
+++ askit/settings.py
@@ -111,6 +111,8 @@ else:
         }
     }
 
+DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
